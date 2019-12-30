efdc6917c7d65f32c33e4d80e334bf55e11e244a
Pierre Veelen <pierre@pvln.nl>
2019-12-30 16:20:17 +0100

    scripts updated

--- get_it.sh
+++ get_it.sh
@@ -7,11 +7,14 @@ fi
 sudo wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/toLIL.conf -O /etc/ipsec.d/toLIL.conf
 
 #get initialization script
-cd ~
-wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/init.sh -O init.sh
+wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/init.sh -O ~/init.sh
 chmod +x init.sh
 
 # get override configuration
 sudo wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/90-override.conf -O /etc/sysctl.d/90-override.conf
+
+#get start script
+wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/start.sh -O ~/start.sh
+chmod +x ~/start.sh
 
 #DONE
--- init.sh
+++ init.sh
@@ -2,11 +2,11 @@
 #
 # set values to disable send_redirects and accept_redirects
 # these settings are gone after reboot
-sudo echo 0 | sudo tee /proc/sys/net/ipv4/conf/*/send_redirects
-sudo echo 0 | sudo tee /proc/sys/net/ipv4/conf/*/accept_redirects
+sudo echo "0" | sudo tee /proc/sys/net/ipv4/conf/*/send_redirects
+sudo echo "0" | sudo tee /proc/sys/net/ipv4/conf/*/accept_redirects
 #
 # set values to enable ip forward
 # these settings are gone after reboot
-sudo echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
+sudo echo "1" | sudo tee /proc/sys/net/ipv4/ip_forward
 
 #DONE
