f876e37a117e8d9614886988af813f2c773f29b7
Pierre Veelen rpi4 <pierre@pvln.nl>
2020-01-01 19:35:34 +0100

    chmodded

--- p/files_get.sh
+++ p/files_get.sh
@@ -5,7 +5,7 @@ cd ~/vpn/p
 sudo cp /etc/ipsec.conf ipsec.conf
 
 #get specifiec ipsec configuration file
-sudo cp /etc/ipsec.d/myconns.conf myconns.conf
+sudo cp /etc/ipsec.d/conn_P-L.conf conn_P-L.conf
 
 # get override configuration
 sudo cp /etc/sysctl.d/90-override.conf 90-override.conf
