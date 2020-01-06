0a823d47612e2967e4000afda6f547e386c61f09
Pierre Veelen <pierre@pvln.nl>
2020-01-06 17:48:09 +0100

    update

--- a/files_get.sh
+++ a/files_get.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd ~/vpn/a
 #get ipsec baseline configuration file
-sudo cp /etc/ipsec.conf ipsec.conf
+sudo cp /etc/ipsec.conf ~/vpn/a/ipsec.conf
 
-#get specifiec ipsec configuration file
-sudo cp /etc/ipsec.d/myconns.conf myconns.conf
+#get specific ipsec configuration file
+sudo cp /etc/ipsec.d/conn_A-L.conf cd ~/vpn/a/conn_A-L.conf
+sudo cp /etc/ipsec.d/conn_A-P.conf cd ~/vpn/a/conn_A-P.conf
 
 #get override configuration
-sudo cp /etc/sysctl.d/90-override.conf 90-override.conf
+sudo cp /etc/sysctl.d/90-override.conf ~/vpn/a/90-override.conf
 
 #DONE
--- a/files_set.sh
+++ a/files_set.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-cd ~/vpn/a
 #set ipsec baseline configuration file
-sudo cp ipsec.conf /etc/ipsec.conf
+sudo cp ~/vpn/a/ipsec.conf /etc/ipsec.conf
 
-#set specifiec ipsec configuration file
-sudo cp myconns.conf /etc/ipsec.d/myconns.conf
+#set specific ipsec configuration file
+sudo cp ~/vpn/a/conn_A-L.conf /etc/ipsec.d/conn_A-L.conf
+sudo cp ~/vpn/a/conn_A-P.conf /etc/ipsec.d/conn_A-P.conf
 
 #set scripts to execution mode
 sudo chmod +x ~/vpn/*.sh
 
 #set override configuration
-sudo cp 90-override.conf /etc/sysctl.d/90-override.conf
+sudo cp ~/vpn/a/90-override.conf /etc/sysctl.d/90-override.conf
 
-sudo cp sysctl.conf /etc/sysctl.conf
+sudo cp ~/vpn/a/sysctl.conf /etc/sysctl.conf
 
 #DONE
