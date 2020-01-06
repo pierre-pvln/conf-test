d7c3224284ee36aa2fd356a3f9c64569a76a052c
Pierre Veelen <pierre@pvln.nl>
2020-01-06 18:21:35 +0100

    update

--- a/get_it.sh
+++ a/get_it.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #get ipsec baseline configuration file
-sudo wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/a/ipsec.conf -O ~/vpn/a/ipsec.conf
+wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/a/ipsec.conf -O ~/vpn/a/ipsec.conf
 
 #get specific ipsec configuration file
-sudo wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/a/conn_A-L.conf -O ~/vpn/a/conn_A-L.conf
-sudo wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/a/conn_A-P.conf -O ~/vpn/a/conn_A-P.conf
+wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/a/conn_A-L.conf -O ~/vpn/a/conn_A-L.conf
+wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/a/conn_A-P.conf -O ~/vpn/a/conn_A-P.conf
 
 #get initialization script
 wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/a/init.sh -O ~/vpn/a/init.sh
 
 # get override configuration
-sudo wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/a/90-override.conf -O ~/vpn/a/90-override.conf
+wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/a/90-override.conf -O ~/vpn/a/90-override.conf
 
 #get start script
 wget -nv https://raw.githubusercontent.com/pierre-pvln/conf-test/master/a/start.sh -O ~/vpn/a/start.sh
