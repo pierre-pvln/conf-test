9f5795187a6da2bf146884b911e01f2e2cbe79f9
Pierre Veelen <pierre@pvln.nl>
2020-01-06 19:47:12 +0100

    update

--- a/files_set.sh
+++ a/files_set.sh
@@ -8,7 +8,7 @@ sudo cp ~/vpn/a/conn_A-L.conf /etc/ipsec.d/conn_A-L.conf
 sudo cp ~/vpn/a/conn_A-P.conf /etc/ipsec.d/conn_A-P.conf
 
 #set scripts to execution mode
-sudo chmod +x ~/vpn/*.sh
+sudo chmod +x ~/vpn/a/*.sh
 
 #set override configuration
 sudo cp ~/vpn/a/90-override.conf /etc/sysctl.d/90-override.conf
